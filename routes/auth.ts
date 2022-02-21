404db3ab06663afb4174cb923d94ac01ae44da9e
Luc H <wereld03@gmail.com>
2022-02-21 09:15:13 +0100

    logic for password resets

--- routes/auth.ts
+++ routes/auth.ts
@@ -3,6 +3,7 @@ import { PrismaClient, User } from '@prisma/client'
 import { Application, Response, Request } from "express";
 const rateLimit = require("express-rate-limit");
 const session = require('express-session');
+const nodemailer = require('nodemailer');
 const csurf = require('csurf');
 const csrf = csurf({ cookie: false })
 const crypto = require("crypto");
@@ -58,12 +59,6 @@ module.exports = function (app: Application, prisma: PrismaClient) {
     // Login Logic
     app.post('/login', authLimit, async function (req: Request, res: Response) {
         const { email, password }: { email: string, password: string } = req.body;
-        if (!validateInput(password)) {
-            res.render('auth/login', {
-                error: "Invalid input"
-            });
-            return;
-        }
 
         if (!validateEmail(email)) {
             res.render('auth/login', {
@@ -112,7 +107,7 @@ module.exports = function (app: Application, prisma: PrismaClient) {
     // Registration logic
     app.post('/register', authLimit, async function (req: Request, res: Response) {
         const { name, email, password, password2 }: { name: string, email: string, password: string, password2: string } = req.body;
-        if (!validateInput(password) || !validateInput(password2)) {
+        if (!validateInput(name)) {
             res.render('auth/register', {
                 error: "Invalid input"
             });
@@ -165,6 +160,123 @@ module.exports = function (app: Application, prisma: PrismaClient) {
         });
     });
 
+    // Request new password
+    app.get('/forgot', function (req: Request, res: Response) {
+        res.render('auth/forgot');
+    });
+
+    // Request new password logic
+    app.post('/forgot', authLimit, async function (req: Request, res: Response) {
+        const { email }: { email: string } = req.body;
+        if (!validateEmail(email)) {
+            res.render('auth/forgot', {
+                error: "Invalid email"
+            });
+            return;
+        }
+        const user = await prisma.user.findFirst({
+            where: {
+                email: email
+            }
+        });
+        if (!user) {
+            res.render('auth/forgot', {
+                error: "User not found"
+            });
+            return;
+        }
+        const token = crypto.randomBytes(16).toString('hex');
+        await prisma.user.update({
+            where: {
+                id: user.id
+            },
+            data: {
+                resetToken: token,
+                resetTokenExpiry: Date.now() + 3600000
+            }
+        });
+        const transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: process.env.SMTP_SECURE, // true for 465, false for other ports
+            auth: {
+              user: process.env.SMTP_USER,
+              pass: process.env.SMTP_PASSWORD, 
+            },
+        });
+        const mailOptions = {
+            from: process.env.SMTP_EMAIL,
+            to: email,
+            subject: 'Password reset',
+            text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+            Please click on the following link, or paste this into your browser to complete the process:\n\n
+            https://${process.env.URL}/reset/${token}\n\n
+
+            If you did not request this, please ignore this email and your password will remain unchanged.\n`
+        };
+        transporter.sendMail(mailOptions, function (err: string, info: any) {
+            if (err) {
+                res.render('auth/forgot', {
+                    error: "An error occured"
+                });
+                return;
+            }
+            res.render('auth/forgot', {
+                success: "An email has been sent to " + email + " with further instructions."
+            });
+        });
+    });
+
+    // Reset password page
+    app.get('/reset/:token', function (req: Request, res: Response) {
+        res.render('auth/reset', {
+            token: req.params.token
+        });
+    });
+
+    // Reset password logic
+    app.post('/reset/:token', authLimit, async function (req: Request, res: Response) {
+        const { password, password2 }: { password: string, password2: string } = req.body;
+        if (password !== password2) {
+            res.render('auth/reset', {
+                error: "Passwords do not match"
+            });
+            return;
+        }
+        const user = await prisma.user.findFirst({
+            where: {
+                resetToken: req.params.token,
+                resetTokenExpiry: {
+                    gt: Date.now()
+                }
+            }
+        });
+        if (!user) {
+            res.render('auth/reset', {
+                error: "Invalid token"
+            });
+            return;
+        }
+        crypto.pbkdf2(password, user.salt + process.env.PASSWORD_PEPPER, 100000, 64, 'sha512', async function (err: string, derivedKey: Buffer) {
+            if(err){
+                res.render('auth/reset', {
+                    error: "An error occured"
+                });
+                return;
+            }
+            await prisma.user.update({
+                where: {
+                    id: user.id
+                },
+                data: {
+                    password: derivedKey.toString('hex'),
+                    resetToken: null,
+                    resetTokenExpiry: null
+                }
+            });
+            res.redirect('/');
+        });
+    });
 
 
 
