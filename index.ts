b05af402532b387a86dcc9462b82a2819a19c6ea
Luc H <wereld03@gmail.com>
2022-02-24 12:40:27 +0100

    finish the logout system.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
 import { Application, NextFunction, Request, Response } from "express";
 import { engine } from 'express-handlebars';
 
@@ -26,21 +26,32 @@ app.use(express.static('public'));
 
 const prisma = new PrismaClient();
 
+declare module 'express-session' {
+  interface SessionData {
+      user: User;
+  }
+}
+
+// Load the authentication subsystem
+if (process.env.AUTH_ENABLED === 'true') {
+  require("./routes/auth.ts")(app, prisma);
+}
+
 // A example that uses the prisma client to retrieve data
 app.get("/", async (req: Request, res: Response) => {
 
   const allUsers = await prisma.user.findMany({
     include: { posts: true },
   });
+
+  if (req.session) {
+    res.render('home', { allUsers, currentUser: req.session.user, auth: process.env.AUTH_ENABLED == 'true' });
+  } else {
+    res.render('home', { allUsers, currentUser: null, auth: process.env.AUTH_ENABLED == 'true' });
+  }
   console.log(allUsers);
-  res.render('home', { allUsers });
 
 });
-
-// Load the authentication subsystem
-if (process.env.AUTH_ENABLED === 'true') {
-  require("./routes/auth.ts")(app, prisma);
-}
 
 // Listen on port the port defined in the .env file
 app.listen(Number(process.env.PORT) ?? 6033, process.env.HOST ?? "0.0.0.0", function () {
